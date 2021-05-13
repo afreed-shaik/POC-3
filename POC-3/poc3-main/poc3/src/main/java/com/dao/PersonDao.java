6ed3c63d7d3676e473ff90aff628fd43f62a569e
afreed-shaik <83745198+afreed-shaik@users.noreply.github.com>
2021-05-13 19:47:41 +0530

    Delete POC-3/poc3-main directory

--- POC-3/poc3-main/poc3/src/main/java/com/dao/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.model.Person;
-
-public interface PersonDao extends MongoRepository<Person, Integer> {
-
-}
